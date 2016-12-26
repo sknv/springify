67f70425d639e8e97e511cb0970c6359a63d6c11
Sergey Kanukov <s.kanukov@gmail.com>
2016-12-26 12:08:24 +0300

    Add index template and a controller to serve the corresponding action

---
+++ src/main/java/com/github/skanukov/springify/controllers/PageController.java
@@ -0,0 +1,15 @@
+package com.github.skanukov.springify.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
