de850a2628389e62fe932627943ecdd4fc170737
Sergey Kanukov <s.kanukov@gmail.com>
2016-12-26 11:11:53 +0300

    Initial commit: start Spring Boot 1.4 app

---
+++ src/main/java/com/github/skanukov/springify/SpringifyApplication.java
@@ -0,0 +1,12 @@
+package com.github.skanukov.springify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringifyApplication.class, args);
+	}
+}
