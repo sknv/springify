2461e5abfd765bc1b824b0b22f60c2c5d1a3b42f
skanukov <s.kanukov@gmail.com>
2017-06-10 13:45:54 +0300

    Remove unnecessary request mapping for the page controller

--- src/main/java/com/github/skanukov/springify/apps/web/controllers/PageController.java
+++ src/main/java/com/github/skanukov/springify/apps/web/controllers/PageController.java
@@ -1,15 +1,12 @@
 package com.github.skanukov.springify.apps.web.controllers;
 
-import com.github.skanukov.springify.apps.web.config.router.WebRouterConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(WebRouterConfig.WEB_BASE_URI + "/")
 public class PageController {
 
     private static final Logger logger = LoggerFactory.getLogger(PageController.class);
