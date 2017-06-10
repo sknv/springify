3eb05496f4d7ee00946baefea10e8857bc36d5d8
skanukov <s.kanukov@gmail.com>
2017-06-10 13:59:51 +0300

    Log user in the api greet controller

--- src/main/java/com/github/skanukov/springify/apps/api/controllers/GreetingController.java
+++ src/main/java/com/github/skanukov/springify/apps/api/controllers/GreetingController.java
@@ -1,7 +1,11 @@
 package com.github.skanukov.springify.apps.api.controllers;
 
 import com.github.skanukov.springify.apps.api.config.router.ApiRouterConfig;
+import com.github.skanukov.springify.core.facades.AuthFacade;
 import com.github.skanukov.springify.lib.models.Greeting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +18,22 @@ import java.util.concurrent.atomic.AtomicLong;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
     private final AtomicLong counter = new AtomicLong();
+    private final AuthFacade authFacade;
+
+    @Autowired
+    public GreetingController(AuthFacade authFacade) {
+        this.authFacade = authFacade;
+    }
 
     @GetMapping("/greet")
-    public Greeting greet(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+        if (authFacade.isAuthenticated()) {
+            logger.info("Greet is accessed by {}", authFacade.getAuthentication().getName());
+        }
+
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
