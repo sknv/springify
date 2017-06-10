a51efb12620e0541178eed3ecb81cf4b51a04908
skanukov <s.kanukov@gmail.com>
2017-06-10 14:36:23 +0300

    Make AuthViewModel class final

--- src/main/java/com/github/skanukov/springify/apps/web/viewmodels/AuthViewModel.java
+++ src/main/java/com/github/skanukov/springify/apps/web/viewmodels/AuthViewModel.java
@@ -2,7 +2,7 @@ package com.github.skanukov.springify.apps.web.viewmodels;
 
 import org.springframework.security.core.Authentication;
 
-public class AuthViewModel {
+public final class AuthViewModel {
 
     public static final String REQUEST_AUTH_ATTR = "auth";
 
